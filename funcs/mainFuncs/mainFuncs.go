c4c4a976ece6d4ee46e34c0dbf939b7c119c728f
amphe <self.fishkid@yandex.ru>
2022-10-12 12:44:22 +0300

    updated

--- funcs/mainFuncs/mainFuncs.go
+++ funcs/mainFuncs/mainFuncs.go
@@ -1,20 +1,14 @@
-package serializationFuncs
+package mainFuncs
 
 import (
-	"encoding/json"
-	"encoding/xml"
 	"errors"
 	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
-	"io"
-	"os"
-	"path/filepath"
 	"strconv"
-	"strings"
-	"testJsonSerDeser/errs"
+	"testJsonSerDeser/funcs/sDsFuncs"
 	s "testJsonSerDeser/structs"
 )
 
@@ -22,138 +16,6 @@ const (
 	w = 450
 	h = 500
 )
-
-func Serialize(folderPath, mode string, arrJ []s.Junior, arrTL []s.TechLeader) (err error) {
-	defer func() { err = errs.WrapIfErr("cant serialize", err) }()
-	if len(arrTL) == 0 && len(arrJ) == 0 {
-		return errors.New("there`s no jun`s and tl`s")
-	}
-	if len(arrTL) != 0 {
-		var (
-			file *os.File
-			tlS  []byte
-		)
-		switch mode {
-		case "json":
-			fPath := filepath.Join(folderPath, "TechLeads"+".json")
-			file, err = os.OpenFile(fPath, os.O_WRONLY|os.O_CREATE, 0644)
-			tlS, err = json.Marshal(arrTL)
-			_, err = file.Write(tlS)
-			err = file.Close()
-		case "xml":
-			fPath := filepath.Join(folderPath, "TechLeads"+".xml")
-			file, err = os.OpenFile(fPath, os.O_WRONLY|os.O_CREATE, 0644)
-			tlS, err = xml.Marshal(arrTL)
-			_, err = file.Write(tlS)
-			err = file.Close()
-		}
-	}
-	if len(arrJ) != 0 {
-		var (
-			file *os.File
-			jS   []byte
-		)
-		switch mode {
-		case "json":
-			fPath := filepath.Join(folderPath, "Juniors"+".json")
-			file, err = os.OpenFile(fPath, os.O_WRONLY|os.O_CREATE, 0644)
-			jS, err = json.Marshal(arrJ)
-			_, err = file.Write(jS)
-			err = file.Close()
-		case "xml":
-			fPath := filepath.Join(folderPath, "Juniors"+".xml")
-			file, err = os.OpenFile(fPath, os.O_WRONLY|os.O_CREATE, 0644)
-			jS, err = xml.Marshal(arrJ)
-			_, err = file.Write(jS)
-			err = file.Close()
-		}
-	}
-	return err
-}
-
-func Deserialize(mode string, entry *widget.Entry, window fyne.Window) (err error) {
-	defer func() { err = errs.WrapIfErr("something went wrong:", err) }()
-	dialog.ShowFileOpen(
-		func(reader fyne.URIReadCloser, err error) {
-			switch mode {
-			case "xml":
-				if !strings.HasSuffix(reader.URI().Path(), ".xml") {
-					dialog.ShowError(errors.New("it`s not a XML file"), window)
-				} else if strings.HasSuffix(reader.URI().Path(), "Juniors.xml") {
-					var (
-						data, _     = io.ReadAll(reader)
-						juniors     []s.Junior
-						sendToEntry []string
-					)
-					err = xml.Unmarshal(data, &juniors)
-					for _, employee := range juniors {
-						str := fmt.Sprintf("Email: %s, Salary %d, Team: %d, Position: %s, Resignation: %d",
-							employee.Email, employee.Salary, employee.Team, employee.Position, employee.Resignation)
-						sendToEntry = append(sendToEntry, str)
-					}
-					final := strings.Join(sendToEntry, "\n")
-					entry.SetText(final)
-				} else if strings.HasSuffix(reader.URI().Path(), "TechLeads.xml") {
-					var (
-						data, _     = io.ReadAll(reader)
-						teamLeaders []s.TechLeader
-						sendToEntry []string
-					)
-					err = xml.Unmarshal(data, &teamLeaders)
-					if err != nil {
-						return
-					}
-					for _, employee := range teamLeaders {
-						str := fmt.Sprintf("Email: %s, Salary: %d, Team: %d, CurrentProject: %s,"+
-							"Position: %s, Subordinates: %d",
-							employee.Email, employee.Salary, employee.Team, employee.CurrentProject,
-							employee.Position, employee.Subordinates)
-						sendToEntry = append(sendToEntry, str)
-					}
-					final := strings.Join(sendToEntry, "\n")
-					entry.SetText(final)
-				}
-			case "json":
-				if !strings.HasSuffix(reader.URI().Path(), ".json") {
-					dialog.ShowError(errors.New("it`s not a JSON file"), window)
-				} else if strings.HasSuffix(reader.URI().Path(), "Juniors.json") {
-					var (
-						data, _     = io.ReadAll(reader)
-						juniors     []s.Junior
-						sendToEntry []string
-					)
-					err = json.Unmarshal(data, &juniors)
-					for _, employee := range juniors {
-						str := fmt.Sprintf("Email: %s, Salary %d, Team: %d, Position: %s, Resignation: %d",
-							employee.Email, employee.Salary, employee.Team, employee.Position, employee.Resignation)
-						sendToEntry = append(sendToEntry, str)
-					}
-					final := strings.Join(sendToEntry, "\n")
-					entry.SetText(final)
-				} else if strings.HasSuffix(reader.URI().Path(), "TechLeads.json") {
-					var (
-						data, _     = io.ReadAll(reader)
-						teamLeaders []s.TechLeader
-						sendToEntry []string
-					)
-					err = json.Unmarshal(data, &teamLeaders)
-					if err != nil {
-						return
-					}
-					for _, employee := range teamLeaders {
-						str := fmt.Sprintf("Email: %s, Salary %d, Team: %d, Position: %s, Subordinates: %d",
-							employee.Email, employee.Salary, employee.Team, employee.Position, employee.Subordinates)
-						sendToEntry = append(sendToEntry, str)
-					}
-					final := strings.Join(sendToEntry, "\n")
-					entry.SetText(final)
-				}
-			}
-		},
-		window,
-	)
-	return err
-}
 
 func DoAction(title string, group *widget.RadioGroup,
 	window fyne.Window, juniors *[]s.Junior, techLeads *[]s.TechLeader,
@@ -323,12 +185,12 @@ func DoAction(title string, group *widget.RadioGroup,
 			}
 		case "parse [from]":
 			if group.Selected == "JSON" {
-				err := Deserialize("json", entry, window)
+				err := sDsFuncs.Deserialize("json", entry, window)
 				if err != nil {
 					dialog.ShowError(err, window)
 				}
 			} else if group.Selected == "XML" {
-				err := Deserialize("xml", entry, window)
+				err := sDsFuncs.Deserialize("xml", entry, window)
 				if err != nil {
 					dialog.ShowError(err, window)
 				}
@@ -338,7 +200,7 @@ func DoAction(title string, group *widget.RadioGroup,
 		case "load [to folder]":
 			if group.Selected == "JSON" {
 				dialog.ShowFolderOpen(func(uri fyne.ListableURI, err error) {
-					err = Serialize(uri.Path(), "json", *juniors, *techLeads)
+					err = sDsFuncs.Serialize(uri.Path(), "json", *juniors, *techLeads)
 					if err != nil {
 						dialog.ShowError(err, window)
 					}
@@ -347,7 +209,7 @@ func DoAction(title string, group *widget.RadioGroup,
 				)
 			} else if group.Selected == "XML" {
 				dialog.ShowFolderOpen(func(uri fyne.ListableURI, err error) {
-					err = Serialize(uri.Path(), "xml", *juniors, *techLeads)
+					err = sDsFuncs.Serialize(uri.Path(), "xml", *juniors, *techLeads)
 					if err != nil {
 						dialog.ShowError(err, window)
 					}
@@ -366,4 +228,11 @@ func DoAction(title string, group *widget.RadioGroup,
 func ResizeAndMove(object fyne.CanvasObject, mvPosX, mvPosy, rSizePosX, rSizePosY float32) {
 	object.Resize(fyne.NewSize(rSizePosX, rSizePosY))
 	object.Move(fyne.NewPos(mvPosX, mvPosy))
+}
+
+func TotalStuff(w *widget.Label, slice1 []s.Junior, slice2 []s.TechLeader) {
+	w.SetText(fmt.Sprintf("Juniors [%d] | techLeads [%d]",
+		len(slice1),
+		len(slice2)),
+	)
 }
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
-	funcs "testJsonSerDeser/serializationFuncs"
+	"testJsonSerDeser/funcs/mainFuncs"
 	s "testJsonSerDeser/structs"
 	"time"
 )
@@ -15,13 +14,6 @@ const (
 	w = 450
 	h = 500
 )
-
-func totalStuff(w *widget.Label, slice1 []s.Junior, slice2 []s.TechLeader) {
-	w.SetText(fmt.Sprintf("Juniors [%d] | techLeads [%d]",
-		len(slice1),
-		len(slice2)),
-	)
-}
 
 func main() {
 	var (
@@ -37,28 +29,31 @@ func main() {
 
 		entry = widget.NewEntry()
 
-		newTechLeadBtn = funcs.DoAction("hire techLead", radio, window, &jStaff, &tLStaff, entry, mainApp)
-		newJuniorBtn   = funcs.DoAction("hire another junior", radio, window, &jStaff, &tLStaff, entry, mainApp)
-		parseBtn       = funcs.DoAction("parse [from]", radio, window, &jStaff, &tLStaff, entry, mainApp)
-		loadBtn        = funcs.DoAction("load [to folder]", radio, window, &jStaff, &tLStaff, entry, mainApp)
+		newTechLeadBtn = mainFuncs.DoAction("hire techLead", radio, window, &jStaff, &tLStaff, entry, mainApp)
+		newJuniorBtn   = mainFuncs.DoAction("hire another junior", radio, window, &jStaff, &tLStaff, entry, mainApp)
+		parseBtn       = mainFuncs.DoAction("parse [from]", radio, window, &jStaff, &tLStaff, entry, mainApp)
+		loadBtn        = mainFuncs.DoAction("load [to folder]", radio, window, &jStaff, &tLStaff, entry, mainApp)
+		fio            = widget.NewLabel("Nazipov Rustam IDB-21-11")
 		itsMe          = widget.NewLabel("telegram @ampheee\n\t\tmgtu stankin")
 
 		total = widget.NewLabel("")
 	)
+	fio.Move(fyne.NewPos(w*0.275, h*0.05))
 	window.CenterOnScreen()
 	window.Resize(fyne.NewSize(w, h))
 	window.SetFixedSize(true)
-	funcs.ResizeAndMove(total, w*0.275, h*0.775, w*0.5, h*0.1)
-	funcs.ResizeAndMove(entry, w*0.1, h*0.5, w*0.8, h*0.25)
-	funcs.ResizeAndMove(itsMe, w*0.31, h*0.825, w*0.5, h*0.1)
+	mainFuncs.ResizeAndMove(total, w*0.275, h*0.775, w*0.5, h*0.1)
+	mainFuncs.ResizeAndMove(entry, w*0.1, h*0.5, w*0.8, h*0.25)
+	mainFuncs.ResizeAndMove(itsMe, w*0.31, h*0.825, w*0.5, h*0.1)
 	radio.Move(fyne.NewPos(w*0.10, h*0.16))
 
-	funcs.ResizeAndMove(newTechLeadBtn, w*0.5, h*0.16, w*0.4, h*0.07)
-	funcs.ResizeAndMove(newJuniorBtn, w*0.5, h*0.23, w*0.4, h*0.07)
-	funcs.ResizeAndMove(parseBtn, w*0.5, h*0.30, w*0.4, h*0.07)
-	funcs.ResizeAndMove(loadBtn, w*0.5, h*0.37, w*0.4, h*0.07)
+	mainFuncs.ResizeAndMove(newTechLeadBtn, w*0.5, h*0.16, w*0.4, h*0.07)
+	mainFuncs.ResizeAndMove(newJuniorBtn, w*0.5, h*0.23, w*0.4, h*0.07)
+	mainFuncs.ResizeAndMove(parseBtn, w*0.5, h*0.30, w*0.4, h*0.07)
+	mainFuncs.ResizeAndMove(loadBtn, w*0.5, h*0.37, w*0.4, h*0.07)
 
 	window.SetContent(container.NewWithoutLayout(
+		fio,
 		radio,
 		newTechLeadBtn,
 		newJuniorBtn,
@@ -72,7 +67,7 @@ func main() {
 
 	go func() {
 		for range time.Tick(time.Millisecond * 500) {
-			totalStuff(total, jStaff, tLStaff)
+			mainFuncs.TotalStuff(total, jStaff, tLStaff)
 		}
 	}()
 
